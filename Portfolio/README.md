ef8f83e340396c319fce7e7b01d8534a0b184d5c
Rawllings Kiprop <rutojr01@gmail.com>
2024-07-12 13:48:23 +0300

    First commit after creating a React App project

---
+++ Portfolio/README.md
@@ -0,0 +1,1 @@
+## This is my Personal Profile Page. It is created with JavaScript library caleed React.
---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center mt-4 text-green-500">
+        RAWLLINGS PORTFOLIO
+      </h1>
+    </div>
+  );
+}
+
+export default App;
